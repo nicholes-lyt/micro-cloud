62c19e47f92c84718f52bfdf038e9d9c8ea93d86
yuting.li@rograndec.com <yuting.li@rograndec.com>
2017-11-14 20:28:10 +0800

    添加访问接口测试

--- micro-cloud-one-web/src/main/java/com/micro/cloud/one/web/controller/HomeController.java
+++ micro-cloud-one-web/src/main/java/com/micro/cloud/one/web/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.micro.cloud.one.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,4 +13,13 @@ public class HomeController {
 	public String index() {
 		return "home/index";
 	}
+	
+	@Value("${server.port}")
+    String port;
+	
+	@ResponseBody
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
 }
