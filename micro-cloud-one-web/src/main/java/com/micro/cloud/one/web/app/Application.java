993c53031a8a2b6b761e000418b30f0d6fa2749c
yuting.li@rograndec.com <yuting.li@rograndec.com>
2017-12-28 20:36:01 +0800

    添加监控服务，springcloud改为release版本

--- micro-cloud-one-web/src/main/java/com/micro/cloud/one/web/app/Application.java
+++ micro-cloud-one-web/src/main/java/com/micro/cloud/one/web/app/Application.java
@@ -1,18 +1,20 @@
 package com.micro.cloud.one.web.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan("com.micro.cloud.one.web")
 @RefreshScope
-public class Application implements WebMvcConfigurer {
+public class Application extends WebMvcConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- micro-cloud-one-web/src/main/java/com/micro/cloud/one/web/config/OneJpaConfig.java
+++ micro-cloud-one-web/src/main/java/com/micro/cloud/one/web/config/OneJpaConfig.java
@@ -3,8 +3,10 @@ package com.micro.cloud.one.web.config;
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,23 +20,30 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.zaxxer.hikari.HikariDataSource;
+
+
+
 @Configuration
 @EnableJpaAuditing
 @EnableTransactionManagement
 @EnableJpaRepositories(entityManagerFactoryRef = "oneEntityManagerFactory", transactionManagerRef = "oneTransactionManager", basePackages = {
 		"com.micro.cloud.one.web.data.repository" })
 public class OneJpaConfig {
+	
+	@Autowired
+	private JpaProperties jpaProperties;
+	
 	@Bean(name = "oneDataSource")
 	@Primary
 	@ConfigurationProperties(prefix = "spring.datasource.one")
 	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
 	}
 
 	@Bean(name = "oneJdbcTemplate")
 	@Primary
 	public JdbcTemplate jdbcTemplate() {
-		System.out.println("springboot 加载数据源：" + dataSource());
 		return new JdbcTemplate(dataSource());
 	}
 
@@ -54,8 +63,7 @@ public class OneJpaConfig {
 	@Bean(name = "oneEntityManagerFactory")
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder) {
 		DataSource dataSource = dataSource();
-
-		return builder.dataSource(dataSource).packages("com.easynetcn.cloud.one.web.data.entity")
+		return builder.dataSource(dataSource).properties(jpaProperties.getHibernateProperties(dataSource)).packages("com.easynetcn.cloud.one.web.data.entity")
 				.persistenceUnit("onePersistenceUnit").build();
 	}
 
